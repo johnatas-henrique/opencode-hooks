87aceb65ec4635c0986a5a961a20572272246b8d
Johnatas Henrique <johnatas.henrique@gmail.com>
2026-04-01 23:13:37 -0300

    feat: add helper modules for script execution and file operations

---
+++ .opencode/plugins/helpers/index.ts
@@ -0,0 +1,11 @@
+import { saveToFile } from "./save-to-file";
+import { runScript } from "./run-script";
+import { appendToSession } from "./append-to-session";
+import { createToast } from "./create-toast";
+import { showToastStaggered, createToastQueue, getGlobalToastQueue, resetGlobalToastQueue } from "./toast-queue";
+import { loadEventsConfig, getEventConfig, resetConfigCache, isEventEnabled } from "./events-config";
+
+
+export { saveToFile, runScript, appendToSession, createToast, showToastStaggered, createToastQueue, getGlobalToastQueue, resetGlobalToastQueue, loadEventsConfig, getEventConfig, resetConfigCache, isEventEnabled };
+
+
