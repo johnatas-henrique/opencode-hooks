a20ade75882fb584db53221533adcc2e0016ee8a
Johnatas Henrique <johnatas.henrique@gmail.com>
2026-04-02 21:52:38 -0300

    refactor: replace hardcoded handlers with modular event system
    - Remove hardcoded event handlers from opencode-hooks.ts
    - Use resolveEventConfig/resolveToolConfig for dynamic config
    - Add tool.execute.before and shell.env hook support
    - Export EventType enum and types from helpers/index.ts

--- .opencode/plugins/helpers/index.ts
+++ .opencode/plugins/helpers/index.ts
@@ -1,11 +1,52 @@
-import { saveToFile } from "./save-to-file";
-import { runScript } from "./run-script";
-import { appendToSession } from "./append-to-session";
-import { createToast } from "./create-toast";
-import { showToastStaggered, createToastQueue, getGlobalToastQueue, resetGlobalToastQueue } from "./toast-queue";
-import { loadEventsConfig, getEventConfig, resetConfigCache, isEventEnabled, type ToastConfig, type ToastVariant, type EventHandlerConfig, type EventsConfig } from "./events-config";
+import { saveToFile } from './save-to-file';
+import { runScript } from './run-script';
+import { appendToSession } from './append-to-session';
+import { createToast } from './create-toast';
+import {
+  showToastStaggered,
+  createToastQueue,
+  getGlobalToastQueue,
+  resetGlobalToastQueue,
+} from './toast-queue';
+import { handlers, type EventHandler } from './handlers';
+import {
+  resolveEventConfig,
+  resolveToolConfig,
+  getHandler,
+  type ResolvedEventConfig,
+} from './events';
+import {
+  EventType,
+  type EventVariant,
+  type EventOverride,
+  type ToolOverride,
+  type EventConfig,
+  type ToolConfig,
+  type UserEventsConfig,
+} from './event-types';
 
-
-export { saveToFile, runScript, appendToSession, createToast, showToastStaggered, createToastQueue, getGlobalToastQueue, resetGlobalToastQueue, loadEventsConfig, getEventConfig, resetConfigCache, isEventEnabled };
-export type { ToastConfig, ToastVariant, EventHandlerConfig, EventsConfig };
-
+export {
+  saveToFile,
+  runScript,
+  appendToSession,
+  createToast,
+  showToastStaggered,
+  createToastQueue,
+  getGlobalToastQueue,
+  resetGlobalToastQueue,
+  handlers,
+  resolveEventConfig,
+  resolveToolConfig,
+  getHandler,
+  EventType,
+};
+export type {
+  EventHandler,
+  ResolvedEventConfig,
+  EventVariant,
+  EventOverride,
+  ToolOverride,
+  EventConfig,
+  ToolConfig,
+  UserEventsConfig,
+};
