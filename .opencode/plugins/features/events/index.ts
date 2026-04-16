7b5d2e184c4ee3dfa5a1bd53ca61506a224edbd3
Johnatas Henrique <johnatas.henrique@gmail.com>
2026-04-16 09:49:28 -0300

    refactor(events): update to use DI resolvers

--- .opencode/plugins/features/events/index.ts
+++ .opencode/plugins/features/events/index.ts
@@ -1,8 +1,33 @@
-export {
-  resolveEventConfig,
-  resolveToolConfig,
-  normalizeInputForHandler,
-  DISABLED_CONFIG,
-  type ResolvedEventConfig,
-} from './events';
+export { normalizeInputForHandler } from './resolvers/normalize-input';
+export { DISABLED_CONFIG } from '../../core/constants';
 export { logEventConfig, logScriptOutput } from './log-event';
+
+export {
+  createContext,
+  createFactory,
+  createEventResolver,
+  createToolResolver,
+} from './context';
+export type {
+  ConfigResolverContext,
+  EventConfigResolver,
+  ToolConfigResolver,
+} from './interfaces';
+
+export { EventConfigResolverImpl } from './resolvers/event-config.resolver';
+export { ToolConfigResolverImpl } from './resolvers/tool-config.resolver';
+
+export { resolveScripts, type ResolvedScripts } from './resolution/scripts';
+export {
+  resolveToastOverride,
+  resolveToastEnabled,
+  resolveDefaultToast,
+} from './resolution/toast';
+export { getBooleanField } from './resolution/boolean-field';
+export {
+  resolveSaveToFile,
+  type ResolvedSaveToFile,
+} from './resolution/save-to-file';
+
+export { resolveEventConfig, resolveToolConfig } from './events';
+export type { ResolvedEventConfig } from './events';
